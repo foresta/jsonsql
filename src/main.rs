e3e2c065b23e377b0c37a85480226d84353d239f
kz_morita <kz.morit@gmail.com>
2021-12-19 16:45:38 +0900

    feat: Impl readlines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let input = readlines();
+    println!("{}", input.join(""));
+}
+
+fn readlines() -> Vec<String> {
+    use std::io::prelude::*;
+
+    let stdin = std::io::stdin();
+    let lines = stdin.lock().lines().map(|x| x.unwrap()).collect();
+    lines
 }
