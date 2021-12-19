6a0b109673bd8404b4f27c2a222c1b22044bf086
kz_morita <kz.morit@gmail.com>
2021-12-19 17:37:07 +0900

    impl: parse json stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+use serde_json::{Result, Value};
+use std::error::Error;
+
 fn main() {
     let input = readlines().join("");
-    println!("{}", input);
+    match parse(&input.to_owned()) {
+        Ok(json) => {
+            println!("{:?}", json);
+            println!("{}", json["glossary"]);
+            println!("{}", json["glossary"]["title"]);
+        }
+        Err(err) => {
+            print_err(&err);
+        }
+    }
+}
+
+fn print_err(err: &dyn Error) {
+    println!("Error occured!: {}", err);
+}
+
+fn parse(source: &str) -> Result<Value> {
+    let v: Value = serde_json::from_str(source)?;
+
+    Ok(v)
 }
 
 fn readlines() -> Vec<String> {
