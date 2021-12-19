818ae20d6126dae7aa24c08437f4df62b69a902b
kz_morita <kz.morit@gmail.com>
2021-12-19 16:50:52 +0900

    data: sample.json

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let input = readlines();
-    println!("{}", input.join(""));
+    let input = readlines().join("");
+    println!("{}", input);
 }
 
 fn readlines() -> Vec<String> {
     use std::io::prelude::*;
 
     let stdin = std::io::stdin();
-    let lines = stdin.lock().lines().map(|x| x.unwrap()).collect();
-    lines
+    stdin.lock().lines().map(|x| x.unwrap()).collect()
 }
